98121d1d13d2c68b5cbf0b4f2efb0929e842aaad
pakdees-lang <pakdee.s@prakklang.ac.th>
2026-06-20 23:47:06 +0700

    Add files via upload
    Update running timer app V10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="running-timer-v9";
+const CACHE="running-timer-v10";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
